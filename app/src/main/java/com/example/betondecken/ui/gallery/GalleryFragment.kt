04b7e2ed80d4ae550a8eec45559861040bb8fb48
Sandro Gallesse <falconph@hotmail.com>
2024-11-05 00:32:57 -0500

    Nuevos cambios

--- app/src/main/java/com/example/betondecken/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/betondecken/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.betondecken.databinding.FragmentGalleryBinding
+import com.example.betondecken.databinding.FragmentAnalyticBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentAnalyticBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAnalyticBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
