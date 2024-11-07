a7cb47f21d10db2f3c8b6c0b59683affa29df792
Sandro Gallesse <falconph@hotmail.com>
2024-11-07 03:18:15 -0500

    Se trabajó con la opción Cuenta, al dar clic nos lleva al perfil del usuario.

--- app/src/main/java/com/example/betondecken/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/betondecken/ui/account/AccountFragment.kt
@@ -1,44 +1,20 @@
+// src/main/java/com/example/betondecken/ui/account/AccountFragment.kt
 package com.example.betondecken.ui.account
 
-import android.accounts.Account
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.betondecken.databinding.FragmentAccountBinding
-import com.example.betondecken.databinding.FragmentSupportBinding
+import com.example.betondecken.R
 
 class AccountFragment : Fragment() {
 
-    private var _binding: FragmentAccountBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val accountViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
-
-        _binding = FragmentAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textAccount
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    ): View? {
+        // Inflar el nuevo diseño del fragmento de cuenta
+        return inflater.inflate(R.layout.fragment_account, container, false)
     }
 }
